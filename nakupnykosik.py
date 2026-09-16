dd59c545218e138087715cda948e0f82f1a02817
Fusky11 <dominikskola6@gmail.com>
2026-09-16 18:55:39 +0200

    Change lists to dictionaries for items and prices

--- nakupnykosik.py
+++ nakupnykosik.py
@@ -1,6 +1,8 @@
-ovocie = ["jablko", "banan", "hruska", "marhula", "slivka"]
-zelenina = ["mrkva", "petrzlen", "celer", "zemiak"]
-sladkosti = ["cokolada", "cukor"]
+ovocie = {"jablko": 1, "banan": 2, "hruska": 2, "marhula": 2, "slivka": 1}
+zelenina = {"mrkva": 1, "petrzlen": 1, "celer": 2, "zemiak": 1}
+sladkosti = {"cokolada": 3, "cukor": 1}
+
+celkova_cena = 0
 
 nakupny_kosik = []
 
@@ -17,7 +19,14 @@ print("-------------------------------------------------------------------------
 for polozka in nakupny_kosik: 
     if polozka in ovocie:
         print(f"{polozka} je ovocie")
+        celkova_cena = celkova_cena + ovocie[polozka]
     elif polozka in zelenina:
         print(f"{polozka} je zelenina")
+        celkova_cena = celkova_cena + zelenina[polozka]
+    elif polozka in sladkosti:
+        print(f"{polozka} je sladkost")
+        celkova_cena = celkova_cena + sladkosti[polozka]
     else:
         print(f"{polozka} je nieco ine")
+
+print(f"celkova cena: {celkova_cena}")
